53423de9291eb6ca32b2d43ddcb353d45361be49
shifeng.liu <LiuShifeng@users.noreply.github.com>
2021-09-06 16:57:39 +1000

    Add files via upload

---
+++ src/model.py
@@ -0,0 +1,42 @@
+from transformers import BertPreTrainedModel, BertModel
+from torch import nn
+from torch.nn import CrossEntropyLoss, MSELoss
+
+
+class PICOSentClassModel(BertPreTrainedModel):
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.bert = BertModel(config, add_pooling_layer=False)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+        self.init_weights()
+
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None,
+                position_ids=None, head_mask=None, inputs_embeds=None, labels=None):
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+        )
+        last_hidden_states = outputs[0]
+        pooled_output = last_hidden_states[:, 0]
+
+        trans_states = self.dropout(pooled_output)
+        logits = self.classifier(trans_states)
+
+        loss = None
+        if labels is not None:
+            if self.num_labels == 1:
+                #  We are doing regression
+                loss_fct = MSELoss()
+                loss = loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+
+        return loss, logits, last_hidden_states, pooled_output
